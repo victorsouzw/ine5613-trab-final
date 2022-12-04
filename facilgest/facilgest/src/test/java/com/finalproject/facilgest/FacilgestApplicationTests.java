611212d06e4af22255d42fb6fb30061625c117d7
victorsouzw <svictorl@hotmail.com>
2022-12-04 00:17:10 -0300

    first commit - add the java project

---
+++ facilgest/facilgest/src/test/java/com/finalproject/facilgest/FacilgestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalproject.facilgest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacilgestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
