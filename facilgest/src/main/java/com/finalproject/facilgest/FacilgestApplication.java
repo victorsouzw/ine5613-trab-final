c6303849160b38015cb077f3dadc39a774bb389c
victorsouzw <svictorl@hotmail.com>
2022-12-04 03:25:57 -0300

    finally the right project, running well

---
+++ facilgest/src/main/java/com/finalproject/facilgest/FacilgestApplication.java
@@ -0,0 +1,13 @@
+package com.finalproject.facilgest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacilgestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacilgestApplication.class, args);
+	}
+
+}
